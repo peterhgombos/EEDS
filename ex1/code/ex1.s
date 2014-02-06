498126f32e59f71da40175a0e1805ba615500d79
Morten Mjelva <morten.mjelva@gmail.com>
2014-02-06 19:16:34 +0100

    Rewrote comments

--- ex1/code/ex1.s
+++ ex1/code/ex1.s
@@ -2,12 +2,12 @@
 
 .include "efm32gg.s"
 
-/////////////////////////////////////////////////////////////////////////////
-//
-// Exception vector table
-// This table contains addresses for all exception handlers
-//
-/////////////////////////////////////////////////////////////////////////////
+/****************************************************************************
+ *                                                                          *
+ * Exception vector table                                                   *
+ * This table contains addresses for all exception handlers                 *
+ *                                                                          *
+ ***************************************************************************/
 
 .section .vectors
 
@@ -71,129 +71,108 @@
 
 .section .text
 
-/////////////////////////////////////////////////////////////////////////////
-//
-// Reset handler
-// The CPU will start executing here after a reset
-//
-/////////////////////////////////////////////////////////////////////////////
+/****************************************************************************
+ *                                                                          *
+ * Reset handler                                                            *
+ * The CPU will start executing here after a reset                          *
+ *                                                                          *
+ ***************************************************************************/
 
 .globl  _reset
 .type   _reset, %function
 .thumb_func
 _reset:
-    // Load  CMU clock
-    // Load CMU base adress
-    ldr r1, =CMU_BASE
-
-    // Load current value of HFPERCLK ENABLE
-    ldr r2, [r1, #CMU_HFPERCLKEN0]
-
-    // Set bit for GPIO clk
+    /* Enable clock to GPIO module */
+    ldr r1, =CMU_BASE                           /* Load the CMU register base address                                   */
+    ldr r2, [r1, #CMU_HFPERCLKEN0]              /* Load the value of High Frequency Peripheral Clock Enable Register 0  */
     mov r3, #1
     lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+    orr r2, r2, r3                              /* Enable the GPIO bit in the CMU_HFPERCLKEN0 register                  */
+    str r2, [r1, #CMU_HFPERCLKEN0]              /* Write the new value to the register                                  */
 
-    orr r2, r2, r3
+
+    /* Configure LED pins
+    ldr r1, =GPIO_PA_BASE                       /* Load the PORT A register base address                                */
+
+    mov r2, #2
+    str r2, [r1, #GPIO_CTRL]                    /* Write 2 to the PORT A Control Register to set high drive strength    */
+
+    mov r2, #0xFFFFFFFF
+    str r2, [r1, #GPIO_DOUT]                    /* Output pins are output low, so we set them high to disable the LEDs  */
+
+    /* Configure input button pins
+    ldr r3, =GPIO_PC_BASE                       /* Load the PORT C register base address                                */
+
+    mov r2, #0x33333333                         /* Set pins 0-7 on PORT C as input                                      */
+    str r2, [r3, #GPIO_MODEL]
+
+    mov r2, #0xff
+    str r2, [r3, #GPIO_DOUT]                    /* Enable internal pull-up on pins 0-7 on PORT C                        */
     
-    // Store new value
-    str r2, [r1, #CMU_HFPERCLKEN0]
+    /* Configure pin change interrupts on PORT C
+    ldr r1, =GPIO_BASE                          /* Load the GPIO register base address                                  */
+    mov r2, #0x22222222                         /* Set the enable bit for all PORTC pins                                */
+    str r2, [r1, #GPIO_EXTIPSELL]               /* Write value to the External Interrupt Port Select Low Register       */
 
+    mov r2, #0xff
 
-    // Configure LED pins
-        // Get PORT A base address
-        ldr r1, =GPIO_PA_BASE
+    str r2, [r1, #GPIO_EXTIFALL]                /* Enable high->low pin change interrupt for PORT C pins                 */
+    str r2, [r1, #GPIO_EXTIRISE]                /* Enable low->high pin change interrupt for PORT C pins                 */
+    str r2, [r1, #GPIO_IEN]                     /* Enable external interrupt generation for PORT C pins                  */
 
-        // Set high drive strength on PORT A
-        mov r2, #2
-        str r2, [r1, #GPIO_CTRL]
-
-
-        // Turn LEDs off
-        mov r2, #0xffffffff
-        str r2, [r1, #GPIO_DOUT]
-
-    // Configure input button pins
-        // Get PORT C base address
-        ldr r3, =GPIO_PC_BASE
-
-        // Set PORT C pins as input
-        mov r2, #0x33333333
-        str r2, [r3, #GPIO_MODEL]
-
-        // Enable internal pull up
-        mov r2, #0xff
-        str r2, [r3, #GPIO_DOUT]
-    
-    // Configure pin interrupts
-        // Configure interrupt source pins
-        ldr r1, =GPIO_BASE
-        mov r2, #0x22222222
-        str r2, [r1, #GPIO_EXTIPSELL]
-
-        mov r2, #0xff
-
-        // Enable high to low level interrupt
-        str r2, [r1, #GPIO_EXTIFALL]
-
-        // Enable low to high level interrupt
-        str r2, [r1, #GPIO_EXTIRISE]
-
-        // Enable interrupt generation
-        str r2, [r1, #GPIO_IEN]
-
-        // Enable interrupt handling
-        ldr r1, =ISER0
-        ldr r2, =#0x802
-        str r2, [r1, #0]
+    ldr r1, =ISER0                              /* Load the Interrupt Set Enable Register 0 address                      */
+    ldr r2, =#0x802                             /* Set the GPIO_EVEN and GPIO_ODD bits                                   */
+    str r2, [r1, #0]                            /* Write the new value to the register                                   */
 
     ldr r1, =SCR
-    mov r2, #6
+    mov r2, #7
     str r2, [r1, #0]
 
+    /* Endless loop */
     LOOP:
-        // Disable PORT A pins output
-        ldr r1, =GPIO_PA_BASE
         mov r2, #0
+
+        /* Disable PORT A pins output in order to save power */
+        ldr r1, =GPIO_PA_BASE
         str r2, [r1, #GPIO_MODEH]
-        // Sleep
+
+        /* Put controller to sleep
         ldr r1, =CMU_BASE
         str r2, [r1, #CMU_LFACLKEN0]
         str r2, [r1, #CMU_LFBCLKEN0]
-        wfi
+        wfi                                     /* Wait for instruction */
         b LOOP
 
-/////////////////////////////////////////////////////////////////////////////
-//
-// GPIO handler
-// The CPU will jump here when there is a GPIO interrupt
-// ///////////////////////////////////////////////////////////////////////////// 
+/****************************************************************************
+ *                                                                          *
+ * GPIO handler                                                             *
+ * The CPU will jump here when there is a GPIO interrupt                    *
+ *                                                                          *
+****************************************************************************/
+
 .thumb_func
 gpio_handler:  
-    // Clear interrupt flag
-    ldr r1, =GPIO_BASE
-    ldr r2, [r1, #GPIO_IF]
-    str r2, [r1, #GPIO_IFC]
+    /* Clear interrupt flag
+    ldr r1, =GPIO_BASE                          /* Load the GPIO register base address          */
+    ldr r2, [r1, #GPIO_IF]                      /* Load the GPIO Interrupt Flag Register        */
+    str r2, [r1, #GPIO_IFC]                     /* Load the GPIO Interrupt Flag Clear Register  */
 
-    // Enable PORT A pins output
+    /* Set pins 8-15 on PORT A as output
     ldr r1, =GPIO_PA_BASE
     mov r2, #0x55555555
     str r2, [r1, #GPIO_MODEH]
 
-    // Write button values to LEDs
-        // Load input and output registers
-        ldr r1, =GPIO_PA_BASE
-        ldr r2, =GPIO_PC_BASE
-        
-        // Copy input values to output
-        ldr r3, [r2, #GPIO_DIN]
-        lsl r3, r3, #8
-        str r3, [r1, #GPIO_DOUT]
+    /* Read button values and set LEDs accordingly
+    ldr r1, =GPIO_PA_BASE                       /* Load the PORT A register base address                                                */
+    ldr r2, =GPIO_PC_BASE                       /* Load the PORT C register base address                                                */
+    ldr r3, [r2, #GPIO_DIN]                     /* Load the button values                                                               */
+    lsl r3, r3, #8                              /* Shift the button values values so they can be written directly the the LED register  */
+    str r3, [r1, #GPIO_DOUT]                    /* Write to the LED register                                                            */
 
-    // Return from interrupt
-    bx lr
+    bx lr                                       /* Return from the interrupt                                                            */
     
 
-/////////////////////////////////////////////////////////////////////////////
+/***************************************************************************/
 
 .thumb_func
 dummy_handler:  
