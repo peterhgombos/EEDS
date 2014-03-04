06fa1f50d3ad3cab610421c5433c84b08f94b018
Morten Mjelva <morten.mjelva@gmail.com>
2014-03-04 14:25:52 +0100

    Tested timer interrupt with incrementing counter

--- ex2/code/interrupt_handlers.c
+++ ex2/code/interrupt_handlers.c
@@ -12,19 +12,18 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
     TODO feed new samples to the DAC
     remember to clear the pending interrupt by writing 1 to TIMER1_IFC
   */  
+  *GPIO_PA_DOUT = ~pins++;
   *TIMER1_IFC = 1;
 }
 
 /* GPIO even pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
 {
-  *GPIO_PA_DOUT = (*GPIO_PC_DIN << 8); /* turn on LEDs D4-D8 (LEDs are active low) */
   *GPIO_IFC = 0xFF; /* Clear interrupt flags */
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
 {
-  *GPIO_PA_DOUT = (*GPIO_PC_DIN << 8); /* turn on LEDs D4-D8 (LEDs are active low) */
   *GPIO_IFC = 0xFF; /* Clear interrupt flags */
 }
