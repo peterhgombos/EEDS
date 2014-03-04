900839c7f99fe552137011f1e69e8bfde10c7450
Morten Mjelva <morten.mjelva@gmail.com>
2014-03-04 14:23:52 +0100

    Implemented button functionality from ex1

--- ex2/code/interrupt_handlers.c
+++ ex2/code/interrupt_handlers.c
@@ -12,20 +12,19 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
     TODO feed new samples to the DAC
     remember to clear the pending interrupt by writing 1 to TIMER1_IFC
   */  
-  //*GPIO_PA_DOUT = 0x0F00; /* turn on LEDs D4-D8 (LEDs are active low) */
-  pins++; /* turn on LEDs D4-D8 (LEDs are active low) */
   *TIMER1_IFC = 1;
 }
 
 /* GPIO even pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
 {
-  *GPIO_PA_DOUT = pins; /* turn on LEDs D4-D8 (LEDs are active low) */
+  *GPIO_PA_DOUT = (*GPIO_PC_DIN << 8); /* turn on LEDs D4-D8 (LEDs are active low) */
   *GPIO_IFC = 0xFF; /* Clear interrupt flags */
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
 {
-  //*GPIO_IFC = 0xFF; /* Clear interrupt flags */
+  *GPIO_PA_DOUT = (*GPIO_PC_DIN << 8); /* turn on LEDs D4-D8 (LEDs are active low) */
+  *GPIO_IFC = 0xFF; /* Clear interrupt flags */
 }
