cfe113225b897159b837733d43d2274be442b437
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-03-08 22:45:05 +0100

    songs now playable, includes airhorn by default

--- ex2/code/songs.h
+++ ex2/code/songs.h
@@ -3,9 +3,9 @@
 
 typedef struct
 {
-  const uint8_t is_stereo;
-  const uint8_t *notes;
-  const uint32_t notes_size;
+  uint8_t is_stereo;
+  uint8_t *notes;
+  uint32_t notes_size;
 } song_t;
 
 void songs_init (void);
@@ -15,5 +15,6 @@ void songs_prev (void);
 void songs_play (void);
 void songs_stop (void);
 void songs_playback (void);
+song_t songs_current_get (void); 
 
 #endif /* SONGS_H__ */
