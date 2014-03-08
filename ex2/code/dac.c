8598c6766fb314a91a18a4a10c89899b9ecab37f
Morten Mjelva <morten.mjelva@gmail.com>
2014-03-08 19:38:24 +0100

    Moved DAC function declarations to dac.h

--- ex2/code/dac.c
+++ ex2/code/dac.c
@@ -3,12 +3,22 @@
 
 #include "efm32gg.h"
 
-void setupDAC()
+void dac_init (void)
 {
-  *CMU_HFPERCLKEN0 |= (1 << 17);        /* Enable clock to DAC */
-
   *DAC0_CTRL |= (1 << 18) || (1 << 16); /* Set prescaler to 2^5 */
   *DAC0_CTRL |= (1 << 4);               /* Set outmode to PIN */
+}
+
+void dac_enable (void)
+{
+  *CMU_HFPERCLKEN0 |= (1 << 17);        /* Enable clock to DAC */
   *DAC0_CH0CTRL |= (1 << 0);            /* Enable channel 0 */
   *DAC0_CH1CTRL |= (1 << 0);            /* Enable channel 1 */
+}
+
+void dac_disable (void)
+{
+  *DAC0_CH0CTRL &= ~(1 << 0);            /* Disable channel 0 */
+  *DAC0_CH1CTRL &= ~(1 << 0);            /* Disable channel 1 */
+  *CMU_HFPERCLKEN0 &= ~(1 << 17);        /* Enable clock to DAC */
 }
