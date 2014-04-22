427ee5463e033ba24cd1330d553eb519b7626294
Morten Mjelva <morten.mjelva@gmail.com>
2014-04-22 20:09:35 +0200

    Lots of work on gamepad driver

--- ex3/src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/src/driver-gamepad-1.0/driver-gamepad.c
@@ -2,98 +2,261 @@
  * This is a demo Linux kernel module.
  */
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/types.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
-#include <linux/device.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/types.h>
+
+#include <asm/siginfo.h>
+#include <asm/uaccess.h>
 
 #include "efm32gg.h"
 
+#define DEVICE_NAME "gamepad"
+
+static struct class *cl;
+static dev_t devno;
+static int use_count;
+
+static void __exit dev_exit(void);
+static int __init dev_init(void);
+static int dev_fasync (int fd, struct file *filp, int mode);
 static int dev_open (struct inode *inode, struct file *filp);
-static int dev_release (struct inode *inode, struct file *filp);
+static int dev_probe (struct platform_device *dev);
 static ssize_t dev_read (struct file *filp, char *buff, size_t len, loff_t *off);
+static int dev_release (struct inode *inode, struct file *filp);
+static int dev_remove (struct platform_device *dev);
+static inline void interrupt_enable (void);
+static inline void interrupt_disable (void);
 
+static const struct of_device_id dev_of_match[] = {
+      { .compatible = "tdt4258", },
+      { },
+};
+MODULE_DEVICE_TABLE (of, dev_of_match);
 
-static struct file_operations my_fops = {
+static struct platform_driver dev_driver = {
+    .probe = dev_probe,
+    .remove = dev_remove,
+    .driver = {
+        .name = "gamepad",
+        .owner = THIS_MODULE,
+        .of_match_table = dev_of_match,
+    },
+};
+
+static struct file_operations f_ops = {
     .owner = THIS_MODULE,
     .read = dev_read,
     .open = dev_open,
-    .release = dev_release
+    .release = dev_release,
+    .fasync = dev_fasync,
 };
 
-static struct cdev my_cdev;
+static struct cdev c_dev;
 
 /* Common GPIO interrupt handle */
 irqreturn_t m_irq (int irq, void *dev_id, struct pt_regs *regs)
 {
+  /* Clear interrupt flags as soon as possible */
   *GPIO_IFC = 0xFF;
-  printk("Interrupt!\n");
+  printk("m_irq()\n");
 
   return IRQ_HANDLED;
 }
 
-/*
- * template_init - function to insert this module into kernel space
- *
- * This is the first of two exported functions to handle inserting this
- * code into a running kernel
- *
- * Returns 0 if successfull, otherwise -1
- */
-
-static int __init template_init(void)
+static void __exit dev_exit(void)
 {
-  printk("Hello World!\n");
+  platform_driver_unregister (&dev_driver);
+}
 
-  /* Initialize char device */
-  cdev_init (&my_cdev, &my_fops);
-  cdev_add (&my_cdev, 0, 1);
+static int __init dev_init(void)
+{
+  return platform_driver_register (&dev_driver);
+}
 
-  /* Configure GPIO interrupts */
-  request_mem_region (GPIO_PA_BASE + 0x100, 28, "gamepad");
-  *GPIO_EXTIPSELL = 0x222; /* Set external interrupts on PORT C */
-  *GPIO_EXTIFALL = 0x7;   /* Enable interrupt on falling edge */
-  release_mem_region (GPIO_PA_BASE + 0x100, 28);
+static int dev_fasync (int fd, struct file *filp, int mode)
+{
+  printk("dev_fasync()\n");
+  return 0;
+}
 
-  /* Release GPIO memory */
-  request_mem_region (GPIO_PC_BASE, 32, "gamepad");
-
-  /* Free IRQs, just in case */
-  free_irq(17, NULL);
-  free_irq(18, NULL);
-
-  /* Register interrupt handler for odd and even GPIO pins */
-  request_irq (17, (irq_handler_t)(m_irq), IRQF_DISABLED, "gamepad", (void *)(m_irq));
-  request_irq (18, (irq_handler_t)(m_irq), IRQF_DISABLED, "gamepad", (void *)(m_irq));
+static int dev_open (struct inode *inode, struct file *filp)
+{
+  printk("dev_open()\n");
+  if (use_count++) {
+    interrupt_enable ();
+  }
 
   return 0;
 }
 
-/*
- * template_cleanup - function to cleanup this module from kernel space
- *
- * This is the second of two exported functions to handle cleanup this
- * code from a running kernel
- */
-
-static void __exit template_cleanup(void)
+static int dev_probe (struct platform_device *pdev)
 {
-  printk("Short life for a small module...\n");
+  int even_irq, odd_irq;
+  int size;
+  struct resource *res;
+
+  printk("dev_probe()\n");
+
+  /* Disable the interrupt before setup */
+  interrupt_disable ();
+
+  /* Set external interrupts on PORT C */
+  *GPIO_EXTIPSELL = 0x22222222;
+  *GPIO_EXTIFALL = 0xFF;
+
+  /* Configure buttons on PORT C */
+  *GPIO_PC_MODEL = 0x33333333;
+  *GPIO_PC_DOUT = 0xFF;
+
+  /* Register interrupt handler for odd and even GPIO pins */
+  even_irq = platform_get_irq (pdev, 0);
+  if (request_irq (even_irq, (irq_handler_t)(m_irq), IRQF_DISABLED, "gamepad", NULL)) {
+      dev_err(&pdev->dev, "gamepad: cannot register IRQ %d\n", even_irq);
+      goto err_even;
+  }
+
+  odd_irq = platform_get_irq (pdev, 1);
+  if (request_irq (odd_irq, (irq_handler_t)(m_irq), IRQF_DISABLED, "gamepad", NULL)) {
+      dev_err(&pdev->dev, "gamepad: cannot register IRQ %d\n", odd_irq);
+      goto err_odd;
+  }
+
+  /* Request GPIO memory region */
+  res = platform_get_resource (pdev, IORESOURCE_MEM, 0);
+  size = resource_size (res);
+  if (!request_mem_region (res->start, size, pdev->name)) {
+      dev_err(&pdev->dev, "gamepad: cannot request GPIO mem region");
+      goto err_res;
+  }
+
+  if (alloc_chrdev_region (&devno, 0, 1, "gamepad") != 0) {
+      dev_err(&pdev->dev, "gamepad: cannot alloc char device region");
+      goto err_chrdev;
+  }
+
+  if ((cl = class_create (THIS_MODULE, "gamepad")) == NULL) {
+      dev_err(&pdev->dev, "gamepad: cannot create class");
+      goto err_class;
+  }
+
+  if (device_create (cl, NULL, devno, NULL, "gamepad") == NULL) {
+      dev_err(&pdev->dev, "gamepad: cannot create char device");
+      goto err_create;
+  }
+
+  cdev_init (&c_dev, &f_ops);
+
+  if (cdev_add (&c_dev, devno, 1) == -1) {
+      dev_err(&pdev->dev, "gamepad: cannot add device");
+      goto err_add;
+  }
   
-  free_irq(1, NULL);
-  release_mem_region (GPIO_PC_BASE, 32);
-  cdev_del (&my_cdev);
+  /* Clear interrupts */
+  *GPIO_IFC = 0xFF;
+
+  interrupt_enable ();
+
+  return 0;
+
+err_add:
+device_destroy (cl, devno);
+
+err_create:
+class_destroy (cl);
+
+err_class:
+unregister_chrdev_region (0, 1);
+
+err_chrdev:
+res = platform_get_resource (pdev, IORESOURCE_MEM, 0);
+size = resource_size (res);
+release_mem_region (res->start, size);
+
+err_res:
+odd_irq = platform_get_irq (pdev, 1);
+free_irq (odd_irq, NULL);
+
+err_odd:
+even_irq = platform_get_irq (pdev, 1);
+free_irq (even_irq, NULL);
+
+err_even:
+  return -1;
+}
+
+static ssize_t dev_read (struct file *filp, char *buff, size_t len, loff_t *off)
+{
+  put_user ('c', buff);
+
+  return (ssize_t) 1;
+}
+
+static int dev_release (struct inode *inode, struct file *filp)
+{
+  printk("dev_release()\n");
+
+  if (--use_count == 0) {
+    interrupt_disable ();
+  }
+
+  return 0;
+}
+
+static int dev_remove (struct platform_device *pdev)
+{
+  int even_irq, odd_irq;
+  int size;
+  struct resource *res;
+
+  dev_notice (&pdev->dev, "dev_remove()\n");
+
+  interrupt_disable ();
+
+  cdev_del (&c_dev);
+  device_destroy (cl, devno);
+  class_destroy (cl);
+
+  /* Unregister char device region */
+  unregister_chrdev_region (0, 1);
+
+  /* Free IRQs */
+  even_irq = platform_get_irq (pdev, 0);
+  free_irq(even_irq, NULL);
+
+  odd_irq = platform_get_irq (pdev, 1);
+  free_irq(odd_irq, NULL);
+
+  /* Release GPIO memory */
+  res = platform_get_resource (pdev, IORESOURCE_MEM, 0);
+  size = resource_size (res);
+  release_mem_region (res->start, size);
+  
+  return 0;
+}
+
+static inline void interrupt_enable (void)
+{
+  *GPIO_IEN = 0xFF; /* Enable external interrupts */
+}
+
+static inline void interrupt_disable (void)
+{
+  *GPIO_IEN = 0x00; /* Disable external interrupts */
 }
 
 
-module_init(template_init);
-module_exit(template_cleanup);
+module_init(dev_init);
+module_exit(dev_exit);
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
-MODULE_LICENSE("BSD");
+MODULE_DESCRIPTION("Driver for the TMA4258 gamepad");
+MODULE_AUTHOR("Morten Mjelva");
+MODULE_LICENSE("Dual BSD/GPL");
