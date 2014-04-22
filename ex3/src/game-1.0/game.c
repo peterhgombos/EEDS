071c5767a3c5a44ba154fdb8a9bb65392094250b
Morten Mjelva <morten.mjelva@gmail.com>
2014-04-22 20:09:49 +0200

    Small test app in game

--- ex3/src/game-1.0/game.c
+++ ex3/src/game-1.0/game.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World, I'm game!\n");
+  int fd;
+  char ch, buf[10];
 
-	exit(EXIT_SUCCESS);
+  printf("Hello World, I'm game!\n");
+
+  fd = open ("/dev/gamepad", O_RDONLY);
+  if (fd == -1) {
+      printf ("Error opening device\n");
+      exit (-1);
+  }
+
+  read (fd, buf, sizeof(buf));
+  printf("Buffer says: %s\n", buf);
+
+  if (close(fd) != 0) {
+      printf("Error closing device\n");
+  }
+
+
+  exit (EXIT_SUCCESS);
 }
