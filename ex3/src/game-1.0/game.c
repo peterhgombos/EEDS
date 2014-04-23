022ea92684be23d24959df2d8466edbfb5941311
Morten Mjelva <morten.mjelva@gmail.com>
2014-04-23 21:54:17 +0200

    Added gamepad init to game

--- ex3/src/game-1.0/game.c
+++ ex3/src/game-1.0/game.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <fcntl.h>
+#include <signal.h>
 #include <unistd.h>
+
+static int gamepad_init (void);
+static void sig_handler (int signo);
+
+static FILE *gamepad;
+
+static void sig_handler (int signo)
+{
+        (void) signo;
+
+        printf("sig_handler()\n");
+        /*printf("%s", fgetc(gamepad));*/
+}
+
+static int gamepad_init (void)
+{
+        if (!(gamepad = fopen ("/dev/gamepad", "rb"))                   ||
+            (signal(SIGIO, &sig_handler) == SIG_ERR)                    ||
+            (fcntl(fileno(gamepad), F_SETOWN, getpid()) == -1)          ||
+            (fcntl(fileno(gamepad), F_SETFL,
+                  fcntl(fileno(gamepad), F_GETFL) | FASYNC) == -1)) {
+                printf("Couldn't initialize gamepad\n");
+                goto err;
+        }
+
+        printf("Gamepad initialized\n");
+
+        return EXIT_SUCCESS;
+err:
+        signal(SIGIO, SIG_DFL);
+        (void) fclose (gamepad);
+
+        return EXIT_FAILURE;
+}
 
 int main(int argc, char *argv[])
 {
-  int fd;
-  char ch, buf[10];
-
-  printf("Hello World, I'm game!\n");
-
-  fd = open ("/dev/gamepad", O_RDONLY);
-  if (fd == -1) {
-      printf ("Error opening device\n");
-      exit (-1);
-  }
-
-  read (fd, buf, sizeof(buf));
-  printf("Buffer says: %s\n", buf);
-
-  if (close(fd) != 0) {
-      printf("Error closing device\n");
-  }
+        printf ("Hello World, I'm game!\n");
 
 
-  exit (EXIT_SUCCESS);
+        /* Open gamepad char device */
+        if (gamepad_init () == EXIT_FAILURE) {
+                exit (EXIT_FAILURE);
+        }
+
+        while(1) {
+                sleep(1);
+        }
+
+        exit (EXIT_SUCCESS);
 }
